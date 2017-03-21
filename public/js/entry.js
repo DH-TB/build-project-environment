7757a77e93a00cd0ef2062a1efd5a96084ae7800
DH-TB <929659475@qq.com>
2017-03-21 15:43:26 +0800

    add css-loader and file-loader

--- public/js/entry.js
+++ public/js/entry.js
@@ -3,7 +3,9 @@ import {render} from 'react-dom';
 
 class Hello extends React.Component{
     render(){
-        return<div>Hello world</div>
+        return<div>Hello world
+        <img src='./img/logo.jpg'/>
+        </div>
     }
 }
 
